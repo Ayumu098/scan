856895872f6a94c48ea52cbaf7f6353d8d52492b
Ayumu098 <stephen.singer.098@gmail.com>
2023-01-30 18:29:24 +0800

    Add gui implementation to scan

--- README.md
+++ README.md
@@ -23,11 +23,11 @@ pip install -r requirements.txt
 
 ## Usage
 
-Run the Python file `scan-gui.py` with arguments `--input=FILEPATH_INPUT` and `--output=FILEPATH_OUTPUT`, where `FILEPATH_INPUT` is the file location of the image to scan and `FILEPATH_OUTPUT` is the file location where the scanned imaged will be saved. If the arguments are not used, the filepaths are both set to the current directory with names `input.png` and `output.png` respectively. Make sure to not include whitespaces in the filepaths.
+Run the Python file `scan-gui.py` with arguments `--input=FILEPATH_INPUT` and `--output=FILEPATH_OUTPUT`, where `FILEPATH_INPUT` is the file location of the image to scan and `FILEPATH_OUTPUT` is the file location where the scanned imaged will be saved. If the arguments are not used, the filepaths are both set to the src directory with names `input.png` and `output.png` respectively. Make sure to not include whitespaces in the filepaths.
 ```
-python src/scan-gui.py --input=input.png --output=output.png
+python src/scan-gui.py --input=src\input.png --output=src\output.png
 ```
 
-This opens a `matplotlib` screen. Drag the four points to the corners of the document in the image. Once the points cover the entire document, close the window. The scanned image will be saved to the output location.
+This opens a `matplotlib` screen with two subplots. The left plot is for the original image and the right plot is a preview of the scanned image. Using the polygon selector, select the four corners of the document in the left plot. Make sure that the first point and the last are connected to form a 4-sided polygon; Else, the program will fail to run. Once the polygon is formed, the right plot will show the scanned image after some delay. Adjust the four points accordingly. If the scanned image is satisfactory, close the window. The scanned image will be saved to the provided output location.
 
 Alternatively, the `scan` function in `scanner.py` can be directly used.
---
+++ src/scan-gui.py
@@ -0,0 +1,76 @@
+"""A GUI application using matplotlib for scanning images using homography.
+"""
+
+import argparse
+import matplotlib.pyplot as plt
+from os.path import isfile
+from matplotlib.widgets import PolygonSelector
+from scan import scan
+
+def get_file_locations() -> tuple[str]:
+    """
+    Obtains the file location of the image to be scanned and
+    and the file location where the scanned imaged will be saved via cli. 
+
+    Returns:
+        tuple[str]: the input_filepath and output_filepath from cli in a tuple
+    """
+
+    parser = argparse.ArgumentParser(prog='Document Scanner GUI Application')
+
+    parser.add_argument('--input',  default="src\input.jpg",
+                        type=str, help="File location of the image to be scanned. Defaults to \"src\input.png\".")
+
+    parser.add_argument('--output', default="src\output.png",
+                        type=str, help="File location where the scanned imaged will be saved. Defaults to \"src\output.png\"")
+
+    arguments = parser.parse_args()
+    input_filepath, output_filepath = arguments.input, arguments.output
+
+    assert isfile(input_filepath), "Invalid load filepath. Please check the file exists and try again."
+
+    return input_filepath, output_filepath
+
+
+def main(input_filepath: str, output_filepath: str):
+    """Opens a matplotlib window with two subplots (left and right), one for the original image in input_filepath and one for the scanned image. The user can use the matplotlib polygon selector to select the portion of the image to be scanned. The scanned image will be displayed in the scanned image subplot. Exiting the window will save the scanned image to the output_filepath.
+
+    Args:
+        input_filepath (str): File location of the image to be scanned.
+        output_filepath (str): File location where the scanned imaged will be saved
+    """
+    
+    # Matplotlib settings
+    figure, ax = plt.subplots(1,2, gridspec_kw = {'wspace':0, 'hspace':0})
+    
+    for a in ax:
+        a.set_xticks([])
+        a.set_yticks([])
+        
+    plt.tight_layout()
+    
+    # Load source image
+    image = plt.imread(input_filepath)
+    
+    ax[0].imshow(image)
+    ax[1].imshow(image)
+    
+    def onselect(verts):
+        """Displays the scanned image in the right subplot when the user adjusts the selector or finishes making the selector.
+
+        Args:
+            verts: List of corners [.x, .y] of the polygon selected by the user. 
+        """
+        scanned_image = scan(image, verts)
+        ax[1].imshow(scanned_image)
+    
+    selector = PolygonSelector(ax[0], onselect)
+    
+    plt.show()
+    
+    # Save image on successful exit
+    plt.imsave(output_filepath, scan(image, selector.verts))
+
+if __name__ == '__main__':
+    args = get_file_locations()
+    main(*args)
